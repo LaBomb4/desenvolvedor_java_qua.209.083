615d70f1709466fb4a236ab2d214d76137eaf557
LaBomb4 <gustavo.a.reis12@aluno.senai.br>
2025-12-10 21:33:15 -0300

    não consegui rodar essa bosta

--- 00_atividades/atividade_09/js/codigo.js
+++ 00_atividades/atividade_09/js/codigo.js
@@ -13,21 +13,40 @@ E mostre na página todos esses dados junto com seu IMC e o seu diagnostico
 
 const form = document.querySelector("form");
 
-const IMC = () => {
-    // Variáveis
+const calcularIMC = (peso, altura) => {
+    let imc = peso / (altura*altura);
+    result = (imc < 18.5) ? " abaixo do peso." : 
+             (imc < 25) ? " peso normal." :
+             (imc < 30) ? " acima do peso." :
+             (imc < 35) ? " obeso." :
+             (imc < 40) ? " obesidade 2." : " vai morrer.";
+
+    return `Seu IMC é ${imc.tofixed(2)}, estando assim, ${result}.`;
+}
+
+const exhibitA = () => {
+    // Declaração de variáveis locais
     let nome = document.querySelector("#nome").value;
-    let idade = document.querySelector("#idade").value;
+    let nascimento = document.querySelector("#nascimento").value;
     let genero = document.querySelector("#genero").value;
     let email = document.querySelector("#email").value;
     let telefone = document.querySelector("#telefone").value;
-    let peso = document.querySelector("#peso").value;
-    let altura = document.querySelector("#altura").value;
-    let sqr = altura * altura;
-    
-    const result = peso / sqr;     
-}
+    let peso = document.querySelector("#peso").value.replace(",", ".");
+    let altura = document.querySelector("#altura").value.replace(",", ".");
 
-    form.addEventListener("submit", function(event){
+    // Saída de dados
+    document.querySelector("#nomeResult").innerHTML = `Nome: $(nome)`;
+    document.querySelector("#nascimentoResult").innerHTML = `Data de Nascimento: $(nascimento)`;
+    document.querySelector("#generoResult").innerHTML = `Gênero: ${genero}`;
+    document.querySelector("#emailResult").innerHTML = `E-mail: ${email}`;
+    document.querySelector("#telefoneResult").innerHTML = `Telefone: ${telefone}`;
+    document.querySelector("#pesoResult").innerHTML = `Peso: ${peso}`;
+    document.querySelector("#alturaResult").value = `Altura: ${altura}`;
+    document.querySelector("#imcResult").innerHTML = calcularIMC(peso, altura);
+
+    // Evento
+    form.addEventListener("submit", function(event) {
         event.preventDefault();
-        IMC();
+        exhibitA();
     });
+}
