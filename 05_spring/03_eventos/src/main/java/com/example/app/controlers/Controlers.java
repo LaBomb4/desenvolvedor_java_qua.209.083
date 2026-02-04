099fb637b5f6a9264b75dadf76885566c130d21e
ead <ead@academico.local>
2026-02-03 21:36:46 -0300

    Bola

--- 05_spring/03_eventos/src/main/java/com/example/app/controlers/Controlers.java
+++ 05_spring/03_eventos/src/main/java/com/example/app/controlers/Controlers.java
@@ -1,21 +1,40 @@
 package com.example.app.controlers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.app.models.Eventos;
+import com.example.app.repository.EventosRepository;
 
 
 @Controller
 public class Controlers {
+    
+    @Autowired
+    private EventosRepository CSR;
+
     @RequestMapping("/")
     public String index(){
         return "index";
     }
-    @RequestMapping("/templates")
-    public String stt(){
-        return "stt.html";
+
+    @RequestMapping(value ="/cadastrarEvento", method=RequestMethod.GET)
+    public String cadatrarEvento(){
+        return "cadastro_evento.html";
+    }
+
+    @RequestMapping (value="/cadastrarEvento", method=RequestMethod.POST)
+    public String cadastrarEvento(Eventos eventos){
+        
+    CSR.save(eventos);
+
+    return "redirect:/";
+    }
+
+    @RequestMapping("/cadastrosucesso")
+    public String cadastrosucesso(){
+        return "cadastro-sucesso";
     }
 }
-
-@Controller
-public class 
