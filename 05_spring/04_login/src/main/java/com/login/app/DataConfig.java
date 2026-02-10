3e3cebbe260247770ade1144382d2cdba2f46d33
LaBomb4 <gustavo.a.reis12@aluno.senai.br>
2026-02-09 21:46:15 -0300

    gfrefg

---
+++ 05_spring/04_login/src/main/java/com/login/app/DataConfig.java
@@ -0,0 +1,38 @@
+package com.login.app;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class DataConfig {
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName("com.mysql.cl.jdbc.Driver");
+        dataSource.setUrl("jdbc.mysql://localhost:3306/db_loguin");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+
+        return dataSource;
+    }
+
+    @Bean
+    public JpaVendorAdapter JpaVendorAdapter(){
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+
+        adapter.setDatabase((Database.MYSQL));
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        adapter.setPrepareConnection(true);
+    
+        return null;
+    }
+
+}
