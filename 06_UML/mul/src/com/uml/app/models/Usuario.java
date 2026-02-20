3e37fbe5069fc9fa9141cdfb3cc72a6fd6a9a111
LaBomb4 <gustavo.a.reis12@aluno.senai.br>
2026-02-19 21:38:26 -0300

    gfrerfg

---
+++ 06_UML/mul/src/com/uml/app/models/Usuario.java
@@ -0,0 +1,61 @@
+package com.uml.app.models;
+
+import java.util.Date;
+
+public class Usuario {
+private long idUsuario;
+private String nome;
+private Date dataNascimento;
+private String email;
+private String senha;
+
+
+    public long getIdUsuario() {
+        return this.idUsuario;
+    }
+
+    public void setIdUsuario(long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataNascimento() {
+        return this.dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return this.senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+
+    public String fazerLogin(String login, String senha){
+        return "bostas";
+    }
+
+    public void fazerLogoff(){
+        
+    }
+}
